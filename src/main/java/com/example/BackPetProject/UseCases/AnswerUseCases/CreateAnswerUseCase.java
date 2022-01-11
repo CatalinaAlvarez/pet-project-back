ad93849bada67b1ea78440ac83e4795211aa0827
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-10 20:45:55 -0500

    Se agrega la función de enviar correo electrónico

--- src/main/java/com/example/BackPetProject/UseCases/AnswerUseCases/CreateAnswerUseCase.java
+++ src/main/java/com/example/BackPetProject/UseCases/AnswerUseCases/CreateAnswerUseCase.java
@@ -3,7 +3,11 @@ package com.example.BackPetProject.UseCases.AnswerUseCases;
 import com.example.BackPetProject.DTO.AnswerDto;
 import com.example.BackPetProject.Mappers.AnswerMapper;
 import com.example.BackPetProject.Repositories.AnswerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.BackPetProject.Repositories.QuestionRepository;
+import com.example.BackPetProject.Repositories.UserRepository;
+import com.example.BackPetProject.UseCases.QuestionUseCases.FindQuestionById;
+import com.example.BackPetProject.UseCases.UserUseCases.FindUserById;
+import com.example.BackPetProject.UseCases.Utils.SendEmailUseCase;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
@@ -14,17 +18,29 @@ public class CreateAnswerUseCase implements CreateAnswer{
 
     private final AnswerRepository answerRepository;
     private final AnswerMapper answerMapper;
+    private final SendEmailUseCase sendEmailUseCase;
+    private final UserRepository userRepository;
+    private final QuestionRepository questionRepository;
 
-    @Autowired
-    public CreateAnswerUseCase(AnswerRepository answerRepository, AnswerMapper answerMapper){
+    public CreateAnswerUseCase(AnswerRepository answerRepository, AnswerMapper answerMapper, SendEmailUseCase sendEmailUseCase, FindUserById findUserById, FindQuestionById findQuestionById, UserRepository userRepository, QuestionRepository questionRepository) {
         this.answerRepository = answerRepository;
         this.answerMapper = answerMapper;
+        this.sendEmailUseCase = sendEmailUseCase;
+        this.userRepository = userRepository;
+        this.questionRepository = questionRepository;
     }
 
 
     @Override
     public Mono<AnswerDto> createAnswer(AnswerDto answerDto) {
-        return answerRepository.save(answerMapper.mapToNewAnswer().apply(answerDto))
-                .map(answerMapper.mapToAnswerDto());
+        return userRepository.findById(
+                questionRepository.findById(answerDto.getParentId()).
+                        map(question -> question.getUserId()))
+                .map(user -> sendEmailUseCase.sendMail(user.getEmail(),
+                        "Han respondido a tu pregunta",
+                        "Un usuario ha respondido a tu pregunta")).
+                then(answerRepository.save(answerMapper.mapToNewAnswer().apply(answerDto))
+                .map(answerMapper.mapToAnswerDto()));
+
     }
 }
