60a734ac4475482c2feb76d8918f865ee566fea0
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-11 00:32:32 -0500

    Se corrige enviar email

--- src/main/java/com/example/BackPetProject/UseCases/AnswerUseCases/CreateAnswerUseCase.java
+++ src/main/java/com/example/BackPetProject/UseCases/AnswerUseCases/CreateAnswerUseCase.java
@@ -2,10 +2,12 @@ package com.example.BackPetProject.UseCases.AnswerUseCases;
 
 import com.example.BackPetProject.DTO.AnswerDto;
 import com.example.BackPetProject.Mappers.AnswerMapper;
+import com.example.BackPetProject.Mappers.QuestionMapper;
 import com.example.BackPetProject.Repositories.AnswerRepository;
 import com.example.BackPetProject.Repositories.QuestionRepository;
 import com.example.BackPetProject.Repositories.UserRepository;
 import com.example.BackPetProject.UseCases.QuestionUseCases.FindQuestionById;
+import com.example.BackPetProject.UseCases.QuestionUseCases.FindQuestionByIdUseCase;
 import com.example.BackPetProject.UseCases.UserUseCases.FindUserById;
 import com.example.BackPetProject.UseCases.Utils.SendEmailUseCase;
 import org.springframework.stereotype.Service;
@@ -21,26 +23,48 @@ public class CreateAnswerUseCase implements CreateAnswer{
     private final SendEmailUseCase sendEmailUseCase;
     private final UserRepository userRepository;
     private final QuestionRepository questionRepository;
+    private final QuestionMapper questionMapper;
+    private final FindQuestionByIdUseCase findQuestionByIdUseCase;
 
-    public CreateAnswerUseCase(AnswerRepository answerRepository, AnswerMapper answerMapper, SendEmailUseCase sendEmailUseCase, FindUserById findUserById, FindQuestionById findQuestionById, UserRepository userRepository, QuestionRepository questionRepository) {
+
+    public CreateAnswerUseCase(AnswerRepository answerRepository, AnswerMapper answerMapper, SendEmailUseCase sendEmailUseCase, FindUserById findUserById, FindQuestionById findQuestionById, UserRepository userRepository, QuestionRepository questionRepository, QuestionMapper questionMapper, FindQuestionByIdUseCase findQuestionByIdUseCase) {
         this.answerRepository = answerRepository;
         this.answerMapper = answerMapper;
         this.sendEmailUseCase = sendEmailUseCase;
         this.userRepository = userRepository;
         this.questionRepository = questionRepository;
+        this.questionMapper = questionMapper;
+        this.findQuestionByIdUseCase = findQuestionByIdUseCase;
     }
 
 
     @Override
-    public Mono<AnswerDto> createAnswer(AnswerDto answerDto) {
-        return userRepository.findById(
-                questionRepository.findById(answerDto.getParentId()).
-                        map(question -> question.getUserId()))
-                .map(user -> sendEmailUseCase.sendMail(user.getEmail(),
-                        "Han respondido a tu pregunta",
-                        "Un usuario ha respondido a tu pregunta")).
-                then(answerRepository.save(answerMapper.mapToNewAnswer().apply(answerDto))
-                .map(answerMapper.mapToAnswerDto()));
+    public Mono<String> createAnswer(AnswerDto answerDto) {
+
+        return userRepository.findById(questionRepository.findById(answerDto.getParentId())
+                        .map(question-> question.getUserId()))
+                            .flatMap(user -> {
+                                return answerRepository.save(answerMapper.mapToAnswer().apply(answerDto))
+                                        .flatMap(answer -> {
+                                            return findQuestionByIdUseCase.findQuestionById(answer.getParentId())
+                                                    .flatMap(questionDTO -> {
+                                                        sendEmailUseCase.sendMail(
+                                                                user.getEmail(),
+                                                                "Han respondido a tu pregunta",
+                                                                "Pregunta: \n" + questionDTO.getQuestionBody() +
+                                                                " \nRespuesta: \n" + answerDto.getAnswerBody());
+                                                        return Mono.just("Correo enviado");
+                                                    });
+                                        });
+
+                            });
+
+
+
+
+
 
     }
+
+
 }
